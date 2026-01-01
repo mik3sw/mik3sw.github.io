7315bacfe2fbf0f2ffa358a79777da770c8b4612
michy <mikedev.xda@gmail.com>
2026-01-01 15:54:59 +0100

    fix?

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-github-pages",
+  basePath: "/mik3sw.github.io",
 
   /**
    * Disable server-based image optimization. Next.js does not support
