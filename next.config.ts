47126e138e0d221e6bd0c90e2dca91c231926be1
michy <mikedev.xda@gmail.com>
2026-01-01 16:04:04 +0100

    fix 2

--- next.config.ts
+++ next.config.ts
@@ -15,12 +15,6 @@ const nextConfig: NextConfig = {
    */
   output: "export",
 
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/mik3sw.github.io",
 
   /**
    * Disable server-based image optimization. Next.js does not support
